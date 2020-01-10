81980264b6fd3b4e515062278d64818d4be20359
Davis Louie <davislouie90@gmail.com>
2020-01-10 09:16:49 -0800

    Created a UI to add key-value pairs to the cookies. Functionality almost complete.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular-Cookies';
+  getKey = '';
+  getValue = '';
+  cookieValue = '';
+
+  constructor(private cookieService: CookieService) {
+  }
+
+  ngOnInit(): void {
+    this.cookieService.set('Test', 'Hello World!');
+    this.cookieValue = this.cookieService.get('Test');
+  }
+
+  getTheValue(): void {
+    this.cookieValue = this.cookieService.get(this.getValue);
+  }
+
+
+
+  showValue(): void {
+    this.cookieValue
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CookieService } from 'ngx-cookie-service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
